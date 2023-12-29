4755a53b5b5497c5d38ca7699c8c11d55c189940
gideonjon <gideonjones63@gmail.com>
2023-12-29 23:38:33 +0100

    activity

--- app/src/main/java/com/example/datepicker/MainActivity.kt
+++ app/src/main/java/com/example/datepicker/MainActivity.kt
@@ -1,11 +1,48 @@
 package com.example.datepicker
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.DatePickerDialog
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var button: Button
+    private val calender = Calendar.getInstance()
+    private lateinit var textView: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        textView = findViewById(R.id.txtSelectDate)
+        button = findViewById(R.id.button)
+
+        button.setOnClickListener {
+            showDatePicker()
+        }
+
+
+    }
+
+    private fun showDatePicker() {
+        val datePickerDialog = DatePickerDialog(
+            this,
+            { DatePicker, year: Int, monthOfYear: Int, dayOfMonth: Int ->
+                val selectDate = Calendar.getInstance()
+                selectDate.set(year, monthOfYear, dayOfMonth)
+                val dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+                val formattedDate = dateFormat.format(selectDate.time)
+                textView.text = "Selected Date" + formattedDate
+            },
+            calender.get(Calendar.YEAR),
+            calender.get(Calendar.MONTH),
+            calender.get(Calendar.DAY_OF_MONTH),
+
+            )
+        datePickerDialog.show()
+
     }
 }
